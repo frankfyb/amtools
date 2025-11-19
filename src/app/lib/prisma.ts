d0c89af3df0437f7d52a5602709b1c18eaa98db6
frannkfyb <frankfyb@163.com>
2025-11-19 09:22:41 +0000

    prisma

--- src/app/lib/prisma.ts
+++ src/app/lib/prisma.ts
@@ -2,6 +2,9 @@ import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
-export const prisma: PrismaClient = globalForPrisma.prisma ?? new PrismaClient();
+const datasourceUrl = process.env.DIRECT_URL;
+
+export const prisma: PrismaClient =
+  globalForPrisma.prisma ?? new PrismaClient({ datasources: { db: { url: datasourceUrl } } });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
